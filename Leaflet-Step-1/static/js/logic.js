a8b84081ceb44fdf1e292793287df740dff5e4f8
Carrie Chunn <crchunn@gmail.com>
2020-09-08 13:03:46 -0500

    setting up files

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,10 @@
+// Set up the tile layer for the map
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "light-v10",
+    accessToken: API_KEY
+  });
+  var API_KEY = config.API_KEY;
+  
+  
